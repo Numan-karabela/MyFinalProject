3f4ec538f796a8529338130fd238e3c8cf099e28
Numan-karabela <104310773+Numan-karabela@users.noreply.github.com>
2024-01-24 15:11:00 +0300

    24.1.2024 appsettions

---
+++ Core/Entities/Concrete/OparationClaim.cs
@@ -0,0 +1,11 @@
+﻿using Core.DataAccess;
+
+namespace Core.Entities.Concrete
+{
+    public class OparationClaim:IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Core/Entities/Concrete/UserOparationClaim.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess;
+
+namespace Core.Entities.Concrete
+{
+    public class UserOparationClaim:IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+
+
+
+    }
+}
